b70b6c6d2b89f086f1c0a141fdcc11ac2404599a
Immonn <tgautam859@gmail.com>
2026-02-12 00:48:54 +0530

    Fixing some minor Bug

--- Frontend/src/components/ShareSidebar.tsx
+++ Frontend/src/components/ShareSidebar.tsx
@@ -9,9 +9,9 @@ import { useNavigate } from "react-router-dom";
 export function ShareSidebar(){
     const navigate=useNavigate()
     return <div className="h-screen bg-white  w-72 fixed left-0 right-0">
-        <div className="flex items-center gap-2 p-4 mt-5">
-            <div className="text-blue-700"><Logo/></div>
-            <div className="text-blue-900 text-4xl font-bold">Brainly</div>
+        <div onClick={()=>navigate("/dashboard")} className="flex items-center gap-2 p-4 mt-5">
+            <div className="text-blue-700 cursor-pointer"><Logo/></div>
+            <div className="text-blue-900 cursor-pointer text-4xl font-bold">Brainly</div>
         </div>
         <div className="pl-5 mt-10 ">
             <SidebarItem onClick={()=>navigate("/share")} text="All" icon={<All></All>}/>
